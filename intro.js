8ed7b65cd6fe22c84e02e4739993f24859eafcde
MANOJ KUMAR <manojkumar06t@gmail.com>
2025-06-22 09:24:56 +0530

    basic ts code

---
+++ intro.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var greeting = 'hello me';
+var myNum = 6;
+greeting.toLowerCase();
+console.log(greeting);
---
+++ intro.ts
@@ -0,0 +1,14 @@
+let greeting : string = 'hello me';
+let myNum = 6;
+
+greeting.toLowerCase();
+
+
+console.log(greeting);
+
+
+//Number
+let userId = 333.55;  
+userId.toFixed();
+
+export {}
